92f32309837b166f96d14a464d9e65e3b5e2cfe5
Surender Kumar <surender7790@gmail.com>
2016-02-03 23:50:03 +0530

    variable.js file added

---
+++ variable.js
@@ -0,0 +1,8 @@
+var integer=123;
+var float=12.78;
+var string ="hello bello";
+var array=["name",123,12.45];
+console.log(integer);
+console.log(float);
+console.log(string);
+console.log(array[0]+array[1]+array[2]);
