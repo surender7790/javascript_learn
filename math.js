245e12be2a55076d3e1a4d5752d772814a67a33d
Surender Kumar <surender7790@gmail.com>
2016-02-04 21:24:04 +0530

    maths and number functionadded

---
+++ math.js
@@ -0,0 +1,70 @@
+/*
+1.abs()
+2.acos()
+3.asin()
+4.atan()
+5.atan2()
+6.ceil()
+7.cos()
+8.E
+9.exp()
+10.floor()
+11.LN10
+12.LN2
+13.log()
+14.LOG10E
+15.LOG2E
+16.max()
+17.MAX_VALUE
+18.MIN_VALUE
+19.NaN
+20.min()
+21.NEGATIVE_INFINITY
+22.PI
+23.POSITIVE_INFINITY
+24.pow()
+25.random()
+26.round()
+27.sin()
+28.sqrt()
+29.SQRT1_2
+30.SQRT2
+31.tan()
+32.toSource()
+33.toExponential()
+34.toFixed()
+35.toPrecision()
+36.toString()
+37.valueOf()
+*/
+
+console.log("Maths and Numbers inBuilt Function");
+var num=-3.43;
+console.log(Math.abs(num));
+console.log("acos(0.99999)= "+Math.acos(0.99999));
+console.log("asin(0.99999)= "+Math.asin(0.99999));
+console.log("atan(0.99999)= "+Math.atan(0.99999));
+//atan2(y,x) returns the angle in x,y points. 
+console.log("atan2(0.99999)= "+Math.atan2(1,0.5));
+console.log("ceil(9.1)= "+Math.ceil(9.1));
+console.log("cos(0.99999)= "+Math.cos(0.99999));
+//Math.E will returns the Euler Number.
+console.log("Math.E= "+Math.E);
+//exp() returns E^x ,where E is Eular number
+console.log("Math.exp(1)= "+Math.exp(1));
+console.log("Math.floor(9.9)= "+Math.floor(9.9));
+//Math.LN10=2.3025
+console.log("LN10 ="+Math.LN10);
+console.log("LN2= "+Math.LN2);
+console.log("log(1000)= "+Math.log(1000));
+console.log("Math.LOG10E= "+Math.LOG10E);
+console.log("Math.LOG2E= "+Math.LOG2E);
+console.log("Math.max(2,5)= "+Math.max(2,5));
+console.log("Number.MAX_VALUE= "+Number.MAX_VALUE);
+console.log("Number.MIN_VALUE= "+Number.MIN_VALUE);
+console.log(isNaN("asdfgh"));
+console.log("Math.min(2,5)= "+Math.min(2,5));
+console.log("Number.NEGATIVE_INFINITY= "+Number.NEGATIVE_INFINITY);
+console.log("Pi= "+Math.PI);
+console.log("Number.POSITIVE_INFINITY= "+Number.POSITIVE_INFINITY);
+console.log("Math.pow(2,3)= "+Math.pow(2,3));
